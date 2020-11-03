9d77780d628390df00946f1ac05f81ec23b3d33c
Krishnatheja Vanka <theja.vanka@gmail.com>
2020-11-04 00:30:04 +0530

    Clean Comments 2

--- Part2/Data_Loaders.py
+++ Part2/Data_Loaders.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 class Nav_Dataset(dataset.Dataset):
     def __init__(self):
         self.data = np.genfromtxt('saved/training_data.csv', delimiter=',', dtype=np.float32)
-# STUDENTS: it may be helpful for the final part to balance the distribution of your collected data
+# it may be helpful for the final part to balance the distribution of your collected data
         # normalize data and save scaler for inference
         self.scaler = MinMaxScaler()
         self.normalized_data = self.scaler.fit_transform(self.data) #fits and transforms
@@ -28,7 +28,7 @@ class Nav_Dataset(dataset.Dataset):
         pickle.dump(self.scaler, open("saved/scaler.pkl", "wb")) #save to normalize at inference
 
     def __len__(self):
-    # STUDENTS: __len__() returns the length of the dataset
+        # __len__() returns the length of the dataset
         return self.n_samples
 
     def __getitem__(self, idx):
@@ -40,8 +40,8 @@ class Nav_Dataset(dataset.Dataset):
             'input': x,
             'label': y,
         }
-# STUDENTS: for this example, __getitem__() must return a dict with entries {'input': x, 'label': y}
-# x and y should both be of type float32. There are many other ways to do this, but to work with autograding
+# for this example, __getitem__() must return a dict with entries {'input': x, 'label': y}
+# x and y should both be of type float32.
 # please do not deviate from these specifications.
 
 
@@ -58,13 +58,13 @@ class Data_Loaders():
             self.test_loader.append(value)
         
 
-# STUDENTS: randomly split dataset into two data.DataLoaders, self.train_loader and self.test_loader
+# randomly split dataset into two data.DataLoaders, self.train_loader and self.test_loader
 # make sure your split can handle an arbitrary number of samples in the dataset as this may vary
 
 def main():
     batch_size = 16
     data_loaders = Data_Loaders(batch_size)
-    # STUDENTS : note this is how the dataloaders will be iterated over, and cannot be deviated from
+    # note this is how the dataloaders will be iterated over, and cannot be deviated from
     for idx, sample in enumerate(data_loaders.train_loader):
         _, _ = sample['input'], sample['label']
     for idx, sample in enumerate(data_loaders.test_loader):
